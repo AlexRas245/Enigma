85b0eae5703350c15ec5d6fe81e79953efeb251a
AlexRas245 <rastikhin2001@mail.ru>
2022-05-22 19:44:23 +0300

    Обработаны первичные исключения. Добавлена функция шифратора/дешифратора

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+def shifrator(string, code, k):
+    lenstr = len(string)
+    lencode = len(code)
+    if k == 1:
+        string = string.ljust(lenstr + (lencode - lenstr % lencode) % lencode, "|")
+        lenstr = len(string)
+    count = int(lenstr / lencode)
+    p1 = 0
+    p2 = lencode
+    string_2 = ''
+    for i in range(count):
+        helpstr = string[p1:p2]
+        for j in code:
+            string_2 += str(helpstr[int(j)])
+        p1 = p2
+        p2 = p2 + lencode
+    if k == 2:
+        while string_2[-1] == '|':
+            string_2 = string_2[:-1]
+    return repr(string_2)
+
+
 print('Здравствуйте, вас приветствует программа "Enigma", я помогу вам зашифровать и расшифровать сообщение.')
 while True:
     print('Хотите начать? \n1 - да,\n0 - выйти.')
@@ -8,10 +30,25 @@ while True:
         if num == '1':
             print('1 - зашифровать сообщение,\n2 - расшифровать сообщение.')
             k = input()
-            if k == '1' or k == '2':
+            try:
+                k = int(k)
+            except ValueError:
+                print('Некорректные данные. Начнем заново.')
+            if k == 1 or k == 2:
                 string = input('Введите сообщение\n')
-                code = input('Введите шифр\n')
+                code = input('Введите шифр - каждое число через пробел\n').split()
+                try:
+                    code = list(map(int, code))
+                except ValueError:
+                    print('Некорректные данные. Шифр должен состоять из цифр.')
+                print(shifrator(string,code,k))
             else:
                 print('Некорректные данные. Начнем заново.')
         else:
             exit()
+
+
+
+# code = input().split()
+# code = list(map(int,code))
+# print(code)
